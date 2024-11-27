db6fe6a41b2ab0b2e8f9cd9661fe9a8bf053ff9a
dasom222g <dasom228@gmail.com>
2024-11-27 17:52:29 +0900

    Add api key

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import * as dotenv from "dotenv";
+import path from "path";
+
+// env 설정
+const __dirname = path.resolve();
+dotenv.config({ path: __dirname + "/.env" });
+
 console.log("Hello world");
+
+console.log(process.env.OPENAI_API_KEY);
+console.log(process.env.PORT);
